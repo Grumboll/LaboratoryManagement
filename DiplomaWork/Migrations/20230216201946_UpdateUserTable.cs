500dd750a530f852a8ddb6dab1dd288f6235fd05
Rumen <rumen_pavlov35@abv.bg>
2023-02-16 22:30:26 +0200

    Add folder migrations for db schema updates

---
+++ DiplomaWork/Migrations/20230216201946_UpdateUserTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiplomaWork.Migrations
+{
+    public partial class UpdateUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "User",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "User",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AddColumn<string>(
+                name: "password_salt",
+                table: "User",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+         name: "password_salt",
+         table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "User",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "User",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
