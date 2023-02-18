868a799167940f7f965adaaf718f926257dd0312
Rumen <rumen_pavlov35@abv.bg>
2023-02-19 00:25:06 +0200

    Create Login Credentials Helper

---
+++ DiplomaWork/Helpers/LoginCredentialsHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace DiplomaWork.Helpers
+{
+    public static class LoginCredentialsHelper
+    {
+        private static readonly byte[] entropy = Encoding.Unicode.GetBytes("your entropy string here");
+
+        public static void SaveLoginCredentials(string username, string password)
+        {
+            try
+            {
+                byte[] encryptedUsername = ProtectedData.Protect(Encoding.UTF8.GetBytes(username), entropy, DataProtectionScope.CurrentUser);
+                byte[] encryptedPassword = ProtectedData.Protect(Encoding.UTF8.GetBytes(password), entropy, DataProtectionScope.CurrentUser);
+                File.WriteAllBytes("login.dat", encryptedUsername.Concat(encryptedPassword).ToArray());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Грешка при запазване на данните!");
+            }
+        }
+
+        public static (string Username, string Password) TryLoadLoginCredentials()
+        {
+            try
+            {
+                byte[] encryptedData = File.ReadAllBytes("login.dat");
+                byte[] encryptedUsername = encryptedData.Take(encryptedData.Length / 2).ToArray();
+                byte[] encryptedPassword = encryptedData.Skip(encryptedData.Length / 2).ToArray();
+
+                string username = Encoding.UTF8.GetString(ProtectedData.Unprotect(encryptedUsername, entropy, DataProtectionScope.CurrentUser));
+                string password = Encoding.UTF8.GetString(ProtectedData.Unprotect(encryptedPassword, entropy, DataProtectionScope.CurrentUser));
+
+                return (username, password);
+            }
+            catch (FileNotFoundException)
+            {
+                return (null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Грешка при зареждане на потребителско име и парола! Моля въведете данните отново.");
+                return (null, null);
+            }
+        }
+    }
+}
--- DiplomaWork/LoginWindow.xaml.cs
+++ DiplomaWork/LoginWindow.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using DiplomaWork.Models;
+using DiplomaWork.Properties;
 using System.Security.Cryptography;
-using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace DiplomaWork
 {
@@ -35,6 +28,7 @@ namespace DiplomaWork
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             string username = UserNameTextBox.Text;
+            string password = PasswordBox.Password;
 
             // Check if the user exists
             var user = _dbContext.Users.FirstOrDefault(u => u.Username == username);
@@ -49,20 +43,31 @@ namespace DiplomaWork
                 // Convert salt and hash from Base64 strings to byte arrays
                 byte[] salt = Convert.FromBase64String(saltString);
                 byte[] hash = Convert.FromBase64String(hashString);
+                byte[] inputHash = Encoding.UTF8.GetBytes(password);
 
                 byte[] passwordBytes = Encoding.UTF8.GetBytes(hash + Convert.ToBase64String(salt));
+                byte[] inputPasswordBytes = Encoding.UTF8.GetBytes(inputHash + Convert.ToBase64String(salt));
+
                 byte[] hashedPassword;
+                byte[] hashedInputPassword;
+
                 using (var sha256 = SHA256.Create())
                 {
                     hashedPassword = sha256.ComputeHash(passwordBytes);
+                    hashedInputPassword = sha256.ComputeHash(inputPasswordBytes);
                 }
 
                 // Compare the resulting hash with the hash stored in the database
-                if (hashedPassword.SequenceEqual(hash))
+                if (hashedPassword.SequenceEqual(hashedInputPassword))
                 {
                     // Authentication successful, open Main Window
                     MainWindow mainWindow = new MainWindow();
                     mainWindow.Show();
+
+                    if(RememberMeCheckBox.IsChecked ?? false)
+                    {
+                        SaveLoginCredentials(username, password);
+                    }
 
                     // Close the current window
                     this.Close();
@@ -87,5 +92,67 @@ namespace DiplomaWork
             base.OnClosed(e);
             _dbContext.Dispose();
         }
+
+        public static void SaveLoginCredentials(string username, string password)
+        {
+            // Convert the username and password to bytes
+            byte[] usernameBytes = Encoding.UTF8.GetBytes(username);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Generate a random salt value
+            byte[] salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // Derive a key from the password and salt using PBKDF2
+            byte[] key = new Rfc2898DeriveBytes(password, salt, 10000).GetBytes(32);
+
+            // Encrypt the username and password with the key
+            byte[] encryptedUsername = ProtectedData.Protect(usernameBytes, key, DataProtectionScope.LocalMachine);
+            byte[] encryptedPassword = ProtectedData.Protect(passwordBytes, key, DataProtectionScope.LocalMachine);
+
+            // Save the encrypted username and password and salt to a file
+            File.WriteAllBytes("login.dat", encryptedUsername.Concat(encryptedPassword).Concat(salt).ToArray());
+        }
+
+        public static bool TryLoadLoginCredentials(out string username, out string password)
+        {
+            username = null;
+            password = null;
+
+            if (!File.Exists("login.dat"))
+            {
+                return false;
+            }
+
+            byte[] encryptedData = File.ReadAllBytes("login.dat");
+            if (encryptedData.Length < 48) // username (16 bytes) + password (16 bytes) + salt (16 bytes)
+            {
+                return false;
+            }
+
+            byte[] encryptedUsername = encryptedData.Take(16).ToArray();
+            byte[] encryptedPassword = encryptedData.Skip(16).Take(16).ToArray();
+            byte[] salt = encryptedData.Skip(32).Take(16).ToArray();
+
+            string savedPassword = Properties.Settings.Default.password;
+
+            // Derive a key from the password and salt using PBKDF2
+            byte[] key = new Rfc2898DeriveBytes(savedPassword, salt, 10000).GetBytes(32);
+
+            // Decrypt the username and password with the key
+            byte[] usernameBytes = ProtectedData.Unprotect(encryptedUsername, key, DataProtectionScope.LocalMachine);
+            byte[] passwordBytes = ProtectedData.Unprotect(encryptedPassword, key, DataProtectionScope.LocalMachine);
+
+            // Convert the decrypted bytes to strings
+            username = Encoding.UTF8.GetString(usernameBytes);
+            password = Encoding.UTF8.GetString(passwordBytes);
+
+            return true;
+        }
     }
+
+
 }
