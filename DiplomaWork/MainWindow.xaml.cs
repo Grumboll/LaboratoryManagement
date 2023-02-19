5af9a11cf7a5cb425d41faa3cb33cef7505b3512
Rumen <rumen_pavlov35@abv.bg>
2023-02-19 10:30:42 +0200

    Add log out button

--- DiplomaWork/MainWindow.xaml.cs
+++ DiplomaWork/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DiplomaWork
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Controllers.CustomWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Сигурни ли сте, че искате да излезете от профила си?", "Излизане", MessageBoxButton.YesNo);
+
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                if (File.Exists("login.dat"))
+                {
+                    File.Delete("login.dat");
+                }
+
+                this.Close();
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+            }
         }
     }
 }
