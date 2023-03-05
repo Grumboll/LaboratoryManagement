5ce7fd9779a2b627867021a395d9793259ccba65
Rumen <rumen_pavlov35@abv.bg>
2023-03-05 17:55:45 +0200

    Add a logging package and finish up laboratory day saving button, Add a label that indicates if save was successful

--- DiplomaWork/Views/LaboratoryDayView.xaml.cs
+++ DiplomaWork/Views/LaboratoryDayView.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Serilog;
 
 namespace DiplomaWork.Views
 {
@@ -132,37 +135,81 @@ namespace DiplomaWork.Views
                 LaboratoryDayDataGrid.ItemsSource = DataItems;
             }
         }
-        
+
         private void LaboratoryDaySave_Click(object sender, RoutedEventArgs e)
         {
             var context = new laboratory_2023Context();
 
-            foreach (LaboratoryDayItem item in DataItems)
+            try
             {
-                //DataItems contains everything from the database with their Ids, If there is a LaboratoryDayItem with no Id, create a new row in the database for it
-                if (item.Id == 0)
+                foreach (LaboratoryDayItem item in DataItems)
                 {
-                    LaboratoryDay newLaboratoryDay = new LaboratoryDay
+                    //DataItems contains everything from the database with their Ids, If there is a LaboratoryDayItem with no Id, create a new row in the database for it
+                    if (item.Id == 0)
                     {
-                        Day = DateOnly.FromDateTime((DateTime)LaboratoryDayDatePicker.SelectedDate),
-                        ProfileId = uint.Parse(item.Profile),
-                        MetersSquaredPerSample = decimal.Parse(item.MetersSquaredPerSample),
-                        PaintedSamplesCount = uint.Parse(item.PaintedSamplesCount),
-                        PaintedMetersSquared = decimal.Parse(item.PaintedMetersSquared),
-                        KilogramsPerMeter = decimal.Parse(item.KilogramsPerMeter),
-                        CreatedAt = DateTime.Now,
-                        UpdatedAt = DateTime.Now,
-                        DeletedAt = null,
-                        CreatedBy = App.CurrentUser.Id,
-                        UpdatedBy = App.CurrentUser.Id,
-                    };
+                        LaboratoryDay newLaboratoryDay = new LaboratoryDay
+                        {
+                            Day = DateOnly.FromDateTime((DateTime)LaboratoryDayDatePicker.SelectedDate),
+                            ProfileId = uint.Parse(item.Profile),
+                            MetersSquaredPerSample = decimal.Parse(item.MetersSquaredPerSample),
+                            PaintedSamplesCount = uint.Parse(item.PaintedSamplesCount),
+                            PaintedMetersSquared = decimal.Parse(item.PaintedMetersSquared),
+                            KilogramsPerMeter = decimal.Parse(item.KilogramsPerMeter),
+                            CreatedAt = DateTime.Now,
+                            UpdatedAt = DateTime.Now,
+                            DeletedAt = null,
+                            CreatedBy = App.CurrentUser.Id,
+                            UpdatedBy = App.CurrentUser.Id,
+                        };
 
-                    context.LaboratoryDays.Add(newLaboratoryDay);
-                    continue;
+                        context.LaboratoryDays.Add(newLaboratoryDay);
+                        continue;
+                    }
+
+                    //In DataItems if a row has been deleted it won't get needlessly updated since it won't be present in the ObservableCollection
+                    var rowToUpdate = context.LaboratoryDays.FirstOrDefault(row => row.Id == item.Id);
+
+                    rowToUpdate.ProfileId = uint.Parse(item.Profile);
+                    rowToUpdate.MetersSquaredPerSample = decimal.Parse(item.MetersSquaredPerSample);
+                    rowToUpdate.PaintedSamplesCount = uint.Parse(item.PaintedSamplesCount);
+                    rowToUpdate.PaintedMetersSquared = decimal.Parse(item.PaintedMetersSquared);
+                    rowToUpdate.KilogramsPerMeter = decimal.Parse(item.KilogramsPerMeter);
+                    rowToUpdate.UpdatedAt = DateTime.Now;
+                    rowToUpdate.UpdatedBy = App.CurrentUser.Id;
+
+                    //Get the difference between LaboratoryDayIds, which is set when loading DataItems, and DataItems that has only ids to be updated
+                    List<uint> idsToDelete = LaboratoryDayIds.Except(DataItems.Select(x => x.Id)).ToList();
+
+                    foreach (uint id in idsToDelete)
+                    {
+                        var rowToDelete = context.LaboratoryDays.FirstOrDefault(x => x.Id == id);
+
+                        //Soft Delete the item from the database
+                        rowToDelete.DeletedAt = DateTime.Now;
+                    }
                 }
+                //270 error Грешка при запазване на промените!
+                context.SaveChanges();
+                SaveLabel.Content = "Успешно запазени промени!";
+                SaveLabel.Foreground = new SolidColorBrush(Colors.Green);
+                var storyboard = (Storyboard)FindResource("FadeInOut");
+                storyboard.Stop();
+                storyboard.Begin(SaveLabel);
             }
+            catch(Exception ex)
+            {
+                context.Database.CurrentTransaction?.Rollback();
 
-            context.SaveChanges();
+                Log.Error(ex, "An error occurred while saving changes.");
+
+                SaveLabel.Content = "Грешка при запазване на промените!";
+                SaveLabel.Foreground = new SolidColorBrush(Colors.Red);
+                var storyboard = (Storyboard)FindResource("FadeInOut");
+                storyboard.Stop();
+                storyboard.Begin(SaveLabel);
+            }
+            
         }
+
     }
 }
